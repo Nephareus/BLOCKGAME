04c4d896bd2f4ae574eb8186c363caca68710fe4
Nephareus <70549122+Nephareus@users.noreply.github.com>
2020-12-01 14:27:35 -0500

    Add files via upload

---
+++ BLOCKGAME/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator anim;
+    private Vector2 vel;
+    private Vector2 direction;
+
+    private void Awake()
+    {
+        {
+            anim.SetBool("isPushing", false);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        {
+            anim.SetFloat("Horizontal", vel.x);
+            anim.SetFloat("Vertical", vel.y);
+            anim.SetFloat("Speed", vel.sqrMagnitude);
+            rb.MovePosition(rb.position + vel * Speed * Time.fixedDeltaTime);
+            if (vel.x != 0)
+            {
+                anim.SetFloat("IdleDirH", vel.x);
+                anim.SetFloat("IdleDirV", 0);
+            }
+            else if (vel.y != 0)
+            {
+
+                anim.SetFloat("IdleDirV", vel.y);
+            }
+            if (vel.y != 0)
+            {
+                anim.SetFloat("IdleDirV", vel.y);
+                anim.SetFloat("IdleDirH", 0);
+            }
+            else if (vel.x != 0)
+            {
+
+                anim.SetFloat("IdleDirH", vel.x);
+            }
+
+
+        }
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        vel = context.ReadValue<Vector2>();
+
+    }
+
+}
+
+
+
+

d01bb30b4a38285c71e2ffabc14acf6af4e4057f
Nephareus <70549122+Nephareus@users.noreply.github.com>
2020-12-02 00:17:43 -0500

    Cleanup

---
+++ BLOCKGAME/Assets/Scripts/PushEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Collider2D))]
+public class PushEvent : MonoBehaviour
+{
+
+    public Animator anim;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            {
+                anim.SetBool("isPushing", true);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            {
+                anim.SetBool("isPushing", false);
+            }
+        }
+
+    }
+}
+
+
---
+++ BLOCKGAME/Assets/Scripts/PushEventPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Collider2D))]
+public class PushEventPlayer : MonoBehaviour
+{
+
+    public Animator anim;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+            {
+                anim.SetBool("isPushing", true);
+            }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+            {
+                anim.SetBool("isPushing", false);
+            }
+    }
+}
+
+
